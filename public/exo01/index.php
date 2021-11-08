ab5243a8d76fd101046bd13cefc4d04b21de3416
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-11-08 10:23:07 +0000

    Exo01

--- public/exo01/index.php
+++ public/exo01/index.php
@@ -1,5 +1,5 @@
 <?php
     date_default_timezone_set("Europe/Paris"); 
     $currentHour = time();
-    echo 'Il est :  '. date('D d/m/Y H:i:s', $currentHour) ."\n";
+    echo 'Il est :  '. date('D d/m/Y H:i:s', $currentHour) ." à Paris.\n";
 ?>
