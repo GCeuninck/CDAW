3923b64543b33d7f92decd44a7eb041c1551f647
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-11-22 15:18:02 +0000

    catalogue_creation_table_media

---
+++ public/catalogue/database/migrations/2021_11_22_145426_create_media_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->increments('id_media');
+            $table->string('title');
+            $table->string('poster_link');
+            $table->string('release_date');
+            $table->string('actors');
+            $table->string('duration');
+            $table->string('genre');
+            $table->string('synopsis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
