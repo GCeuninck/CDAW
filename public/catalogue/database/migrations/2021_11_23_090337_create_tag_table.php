6d0e55b400ba6a4276dcd5d399fd123dbd37411e
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-11-23 09:13:03 +0000

    catalogue_create_tag_table

---
+++ public/catalogue/database/migrations/2021_11_23_090337_create_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag', function (Blueprint $table) {
+            $table->string('type_tag');
+            $table->integer('code_tag');
+            $table->bigInteger('id_media_tag');
+            $table->timestamps();
+            $table->primary(['type_tag', 'code_tag', 'id_media_tag']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag');
+    }
+}
