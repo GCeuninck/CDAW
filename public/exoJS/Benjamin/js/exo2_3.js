5865647111b2f9a58b7c04ab14b68fba7e7f64c9
LAPTOP-KUR9BG7K\benja <benjamin.leroux59@gmail.com>
2021-11-23 15:27:55 +0000

    exo 2.3 (Ben)

--- public/exoJS/Benjamin/js/exo2_3.js
+++ public/exoJS/Benjamin/js/exo2_3.js
@@ -2,16 +2,23 @@
 
 function modify(e)
 {
-    alert(e.type +" on modify for "+ e.currentTarget.parentNode.id+" !");
+    var mainDiv = document.getElementById(e.currentTarget.parentNode.id),
+    childDiv = mainDiv.getElementsByTagName('p')[0];
+
+    childDiv.textContent="Modified";
 }
 
 function deleter(e)
 {
-    alert(e.type +" on remove for "+ e.currentTarget.parentNode.id+" !");
+    document.getElementById( e.currentTarget.parentNode.id).remove();
 }
 
 document.getElementById("addNew").addEventListener("click", function(e){
-    alert(e.type +" on add !");
+    var html_to_insert = "<div id='user"+
+    + (parseInt(users.lastElementChild.id.substring(4)) + 1).toString()
+    +"'><h4>Yumi Sawamura</h4><p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><button class='modify'>Modify Comment</button><button class='remove'>Remove Comment</button></div> ";
+
+    document.getElementById('users').insertAdjacentHTML('beforeend', html_to_insert);
 })
 
 let modifiers = document.getElementsByClassName("modify");
