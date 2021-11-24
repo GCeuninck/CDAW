61e89e2012342b9331bb20b5d8b11b9cc2aea829
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-11-24 09:35:21 +0000

    JS_DOM_exo4

---
+++ public/exoJS/Guillaume_CEUNINCK/DOM/js/scriptsExo4.js
@@ -0,0 +1,55 @@
+"use strict";
+
+function modify(e)
+{
+    document.getElementById(e.currentTarget.parentNode.id).getElementsByTagName('p')[0].textContent = "Chaîne modifiée!!";
+    document.forms.myForm["input"][0].value = "test";
+    
+}
+
+function deleter(e)
+{
+    document.getElementById(e.currentTarget.parentNode.id).remove();
+}
+
+document.getElementById("addNew").addEventListener("click", function(e){
+    let users = document.getElementById("users");
+    let newId;
+
+    if(users.lastElementChild != null)
+    {
+        newId = parseInt(users.lastElementChild.id.substring(4)) + 1;
+    }
+    else
+    {
+        newId = 1;
+    }
+    let title = document.createElement('h4');
+    title.innerHTML = "Bloc ajouté";
+
+    let paragraph = document.createElement('p');
+    paragraph.innerHTML = "Commentaire ajouté";
+
+    let buttonEdit = document.createElement('button');
+    buttonEdit.innerHTML = "Modify Comment";
+    buttonEdit.className = "modify";
+
+    let buttonRemove = document.createElement('button');
+    buttonRemove.innerHTML = "Remove Comment";
+    buttonRemove.className = "remove";
+
+    let content = document.createElement('div');
+    content.id = "user" + newId;
+    content.append(title);
+    content.append(paragraph);
+    content.append(buttonEdit);
+    content.append(buttonRemove);
+
+    users.appendChild(content);
+})
+
+let modifiers = document.getElementsByClassName("modify");
+Array.from(modifiers).forEach(m => m.addEventListener("click",modify));
+
+let remover = document.getElementsByClassName("remove");
+Array.from(remover).forEach(m => m.addEventListener("click",deleter));
