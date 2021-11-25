bfba7a335289834ada883194be6fed752ffea0c9
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-11-25 14:36:56 +0000

    catalogue_edit_connexion_blade

--- public/catalogue/resources/views/auth/forgot-password.blade.php
+++ public/catalogue/resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('Vous avez oublié votre mot de passe ? Aucun problème. Renseignez votre adresse email et nous vous enverrons un email contenant un lien pour choisir un nouveau mot de passe.') }}
         </div>
 
         @if (session('status'))
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Réinitialiser mon mot de passe') }}
                 </x-jet-button>
             </div>
         </form>
