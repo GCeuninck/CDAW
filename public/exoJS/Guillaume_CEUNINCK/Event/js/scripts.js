cf30fe4926602f07c4fc444040a90b017ecce176
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-11-26 09:23:47 +0000

    JS_Event_Intro

---
+++ public/exoJS/Guillaume_CEUNINCK/Event/js/scripts.js
@@ -0,0 +1,19 @@
+"use strict";
+function buttonEvent(e){ 
+
+    alert("Evénement déclenché par : " + e.target.innerHTML);
+    if(e.target == b1){
+        b2.addEventListener("click", buttonEvent);
+        b1.removeEventListener("click", buttonEvent);
+    }
+    else if(e.target == b2){
+        b1.addEventListener("click", buttonEvent);
+        b2.removeEventListener("click", buttonEvent);
+    }
+    stopPropagation();
+};
+
+var b1 = document.getElementById("b1");
+var b2 = document.getElementById("b2");
+
+b1.addEventListener("click", buttonEvent);
