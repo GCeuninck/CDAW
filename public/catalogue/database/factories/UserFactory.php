231511e92bda9e40843f2cd6c5da2a81e53ba78f
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2021-11-28 11:12:17 +0000

    fix User factory for test

--- public/catalogue/database/factories/UserFactory.php
+++ public/catalogue/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Team;
 use App\Models\User;
+use App\Models\KeyValue;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
@@ -24,11 +25,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        if(KeyValue::getUserRole() == null){
+            KeyValue::createUserRole();
+        }
+
         return [
-            'name' => $this->faker->name(),
+            'pseudo' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'code_role' => KeyValue::getUserRole()['code'],
             'remember_token' => Str::random(10),
         ];
     }
@@ -61,7 +67,7 @@ class UserFactory extends Factory
         return $this->has(
             Team::factory()
                 ->state(function (array $attributes, User $user) {
-                    return ['name' => $user->name.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
+                    return ['pseudo' => $user->pseudo.'\'s Team', 'user_id' => $user->id, 'personal_team' => true];
                 }),
             'ownedTeams'
         );
