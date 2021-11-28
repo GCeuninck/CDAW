416bc3ea32b59f6f7bce36913142314eb6b4f296
LAPTOP-KUR9BG7K\benja <benjamin.leroux59@gmail.com>
2021-11-28 07:36:36 +0000

    titre onglet & logo

--- public/catalogue/resources/views/layouts/app.blade.php
+++ public/catalogue/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Binge Watch</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
         <link href="{{asset('../resources/css/styles.css')}}" rel="stylesheet" />
-
+        <link rel="icon" type="image/x-icon" href="{{asset('../resources/assets/logo.ico')}}" />
         @livewireStyles
 
         <!-- Scripts -->
