46fbfd8eaaecd1f2896742ddd9865ab5cc6ba0ca
LAPTOP-KUR9BG7K\benja <benjamin.leroux59@gmail.com>
2021-11-28 11:33:32 +0000

    playlists blade

---
+++ public/catalogue/resources/views/playlists.blade.php
@@ -0,0 +1,11 @@
+@extends("template")
+
+@section("contentBody")
+    <div class="container">
+        <h1>Playlists</h1>
+        <p style="color: red">EN COURS DE CONSTRUCTIONS</p>
+        <br>
+        <p>Cette route est protégée par un middleware ! Elle n'est visible que si vous êtes connecté ;)</p>
+
+    </div>
+@endsection
