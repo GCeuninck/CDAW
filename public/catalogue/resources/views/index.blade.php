0ff8b96a9705cdb6b592fc987b70134242ebfa3b
LAPTOP-KUR9BG7K\benja <benjamin.leroux59@gmail.com>
2021-12-01 16:55:05 +0000

    add voir plus link

--- public/catalogue/resources/views/index.blade.php
+++ public/catalogue/resources/views/index.blade.php
@@ -4,7 +4,12 @@
 
 <!-- Films Section -->
 <div class="container px-4 top-1">
-    <h1>Films</h1>
+    <div class="header-align">
+        <h1>Films</h1>
+        <a href="{{ url('/all/movies' ) }}">
+            <span> Voir tous les films</span>
+        </a>
+    </div>
     <div class="row">
         <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
             <div class="MultiCarousel-inner">
@@ -30,8 +35,13 @@
 <hr/>
 
 <!-- Series Section-->
-<div class="container px-4">
-    <h1>Séries</h1>
+<div class="container px-4 bottom-1">
+    <div class="header-align">
+        <h1>Séries</h1>
+        <a href="{{url('/all/series' ) }}">
+            <span> Voir toutes les séries</span>
+        </a>
+    </div>
     <div class="row">
         <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
             <div class="MultiCarousel-inner">
